1556ffe0446b6aa3aa848a2413a03f678182637d
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-05-02 16:46:30 +0545

    Program to display the product of two variables

---
+++ Labsheet 1/program3.c
@@ -0,0 +1,14 @@
+// Program to display the product of 10 & 8
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    // Declaring th variable a and b for 10 and 8 respectivly also declaring prod as product
+    int a = 10;
+    int b = 8;
+    int prod;
+    // doing calculation by variables
+    prod = a * b;
+    printf("The Product is : %d", prod);
+    return 0;
+}
