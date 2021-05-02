41e1e683522e96184011455ad0219b649fb57cd8
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-05-02 16:45:55 +0545

    Program to display the sum of two variables

---
+++ Labsheet 1/program2.c
@@ -0,0 +1,14 @@
+// Program to display the sum of 5 & 7
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    // Declaring th variable a and b for 5 and 7 respectivly also declaring sum
+    int a = 5;
+    int b = 7;
+    int sum;
+    // doing calculation by variables
+    sum = a + b;
+    printf("The Sum is : %d", sum);
+    return 0;
+}
