46973cecaba58c9abfdf932a9e17d61d0ddf52d5
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-05-11 21:47:11 +0545

    Program to maintain variable in order

---
+++ Labsheet 1/program7.c
@@ -0,0 +1,13 @@
+// Program to maintain variable in order
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter a ,b,c");
+scanf("%d%d%d",&a,&b,&c);
+printf("the forward order:%d,%d,%d",a,b,c);
+printf("the reverse order:%d%d%d",c,b,a);
+getch();
+return 0;
+}
