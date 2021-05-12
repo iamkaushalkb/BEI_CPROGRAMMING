7a8ddc3a1f1e63d5bf76e5436cebbed01dba2a9e
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-05-12 09:02:17 +0545

    Alternative Program to find Size

---
+++ Labsheet 2/Alternative/program4.c
@@ -0,0 +1,29 @@
+
+// C program to find the size of int, char,
+#include <stdio.h>
+  
+int main()
+{
+    int integerType;
+    char charType;
+    float floatType;
+    double doubleType;
+    long double longdoubleType;
+  
+    printf("Size of int is: %ld\n",
+           sizeof(integerType));
+  
+    printf("Size of char is: %ld\n",
+           sizeof(charType));
+  
+    printf("Size of float is: %ld\n",
+           sizeof(floatType));
+  
+    printf("Size of double is: %ld\n",
+           sizeof(doubleType));
+
+    printf("Size of long double is: %ld\n",
+           sizeof(longdoubleType));
+  
+    return 0;
+}
