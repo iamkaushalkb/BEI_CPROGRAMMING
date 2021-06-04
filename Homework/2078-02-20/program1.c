081864dbf424fac9768a462ba0f66770f1a5a0bb
Kaushal Kumar Bhattarai <iamkaushalkb@localhost.localdomain>
2021-06-04 09:29:52 +0545

    Program to calculate and display the following series: 2+4+6+…… upto n terms

--- Homework/2078-02-20/program1.c
+++ Homework/2078-02-20/program1.c
@@ -1,15 +1,17 @@
 // Program to calculate the following series: 2+4+6+…… upto n terms
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int n,sum=0,i,term=2;
-	printf("\nEnter n : ");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++) 
-	{
-		sum+=term; 
-		term+=2;  
-	}
-	printf("\nThe sum is: %d",sum);
-	return 0;
+    int n, sum = 0, i, term = 2;
+    printf("\nEnter n:\t");
+    scanf("%d", &n);         //2
+    for (i = 1; i <= n; i++) //3<=2
+    {
+        printf("%d\t", term);
+        sum += term; //sum = sum+ term// sum = 0+2=2//sum = 2+4=6
+        term += 2;   // term=2+2=4//term=6
+    }
+    printf("\nThe sum is:\t%d", sum);
+    return 0;
+}return 0;
 }
