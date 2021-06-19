5cc63fa9bd834d5778c44be44db997b3d11ed007
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-06-19 15:41:26 +0000

    Program to Count Positive and Negative Numbers in an Array

---
+++ Classwork/2078-03-04/program4.c
@@ -0,0 +1,39 @@
+/* C Program to Count Positive and Negative Numbers in an Array */
+#include <stdio.h>
+
+int main()
+{
+    int Size, i, a[10];
+    int Positive_Count = 0, Negative_Count = 0, Zero_Count = 0;
+
+    printf("\n Please Enter the Size of an Array :  ");
+    scanf("%d", &Size);
+
+    printf("\nPlease Enter the Array Elements\n");
+    for (i = 0; i < Size; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < Size; i++)
+    {
+        if (a[i] > 0)
+        {
+            Positive_Count++;
+        }
+        else if (a[i] < 0)
+        {
+            Negative_Count++;
+        }
+
+        else
+        {
+            Zero_Count++;
+        }
+    }
+
+    printf("\n Total Number of Positive Numbers in this Array = %d ", Positive_Count);
+    printf("\n Total Number of Negative Numbers in this Array = %d ", Negative_Count);
+    printf("\n Total Number of Zero Numbers in this Array = %d \n", Zero_Count);
+    return 0;
+}
