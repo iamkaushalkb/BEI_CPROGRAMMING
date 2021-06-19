21cf4db5f937798273a25b6435b6df5d81505c22
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-06-19 15:44:54 +0000

    Program to Print Tiangular number between two positive integers

---
+++ Labsheet 5/program2.c
@@ -0,0 +1,35 @@
+// Program to Print Tiangular number between two positive integers
+#include <stdio.h>
+int main()
+{
+    int n1, n2, sum = 0, i, j, check;
+    printf("Enter two positive numbers for range:");
+    scanf("%d%d", &n1, &n2);
+    printf("The triangular numbers are:");
+    for (i = n1; i <= n2; i++)
+    {
+        sum = 0;
+        check = 0;
+        j = 1;
+        while (1)
+        {
+            sum = sum + j;
+            if (sum == i)
+            {
+                check = 1;
+                break;
+            }
+            else if (sum > i)
+            {
+                break;
+            }
+            j++;
+        }
+        if (check == 1)
+        {
+            printf("%d\t", i);
+        }
+    }
+
+    return 0;
+}
