fe1af2860818478217e9bb008a13706cfb50ba4c
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-06-19 15:49:26 +0000

    Program to convert decimal to binary number using function

---
+++ Labsheet 5/program6.c
@@ -0,0 +1,26 @@
+// Program to convert decimal to binary number using function
+#include <stdio.h>
+#include <math.h>
+
+long decimalToBinary(int decimalnum)
+{
+    long binarynum = 0;
+    int rem, temp = 1;
+    while (decimalnum != 0)
+    {
+        rem = decimalnum % 2;
+        decimalnum = decimalnum / 2;
+        binarynum = binarynum + rem * temp;
+        temp = temp * 10;
+    }
+    return binarynum;
+}
+
+int main()
+{
+    int decimalnum;
+    printf("Enter a Decimal Number: ");
+    scanf("%d", &decimalnum);
+    printf("Equivalent Binary Number is: %ld", decimalToBinary(decimalnum));
+    return 0;
+}
